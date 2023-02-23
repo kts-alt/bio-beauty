7453e458334477e17e7bdaaabd216c2e31820d52
kts-alt <kdillay1@gmail.com>
2023-02-23 10:38:53 -0500

    added images and links

--- dfs.py
+++ dfs.py
@@ -10,8 +10,3 @@ img_path = "static/images/saved_img.jpg"
 actions = ['age', 'gender', 'race', 'emotion']
 
 objs = DeepFace.analyze(img_path, actions, enforce_detection=False)
-
-if objs[0]['dominant_gender'] == 'Man':
-    print("We've got a guy over here!")
-else:
-    print("We've guy a girl over here!")
